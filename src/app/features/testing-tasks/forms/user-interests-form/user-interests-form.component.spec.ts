a4ba82ef7bf4bb793aecf51f94b00b0ad01ca549
pwoloszun <piotr.woloszun@gmail.com>
2021-10-13 13:29:43 +0200

    change ng mat select value

--- src/app/features/testing-tasks/forms/user-interests-form/user-interests-form.component.spec.ts
+++ src/app/features/testing-tasks/forms/user-interests-form/user-interests-form.component.spec.ts
@@ -16,6 +16,18 @@ describe('UserInterestsFormComponent', () => {
   fit('should toggle render Details group', async () => {
     await renderComponent();
 
+    const selectEl = screen.getByLabelText(/Select Your Country/i);
+    await selectOptionFrom(selectEl, /USA/i);
+
+    const changedSelectEl = screen.getByLabelText(/Select Your Country/i);
+    expect(changedSelectEl).toHaveTextContent(/USA/i);
+
+    // user
+    // userEvent.click(el);
+    // screen.getByRole('textbox', { name: 'Your name?', hidden: true });
+
+    // screen.getByPlaceholderText(/Your name\?/i, { exact: false });
+
     // expect(true).toEqual(false);
   });
 
@@ -24,6 +36,12 @@ describe('UserInterestsFormComponent', () => {
   });
 
 });
+
+async function selectOptionFrom(selectEl: HTMLElement, optionText: string | RegExp) {
+  userEvent.click(selectEl);
+  const optionEl = await screen.findByText(optionText);
+  userEvent.click(optionEl);
+}
 
 async function renderComponent() {
   return render(UserInterestsFormComponent, {
