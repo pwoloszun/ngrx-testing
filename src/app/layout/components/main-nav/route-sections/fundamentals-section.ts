e50e49435ace941011dda7d504d381e6af17f407
pwoloszun <piotr.woloszun@gmail.com>
2021-10-13 14:41:37 +0200

    ngrx fundamentals

--- src/app/layout/components/main-nav/route-sections/fundamentals-section.ts
+++ src/app/layout/components/main-nav/route-sections/fundamentals-section.ts
@@ -2,8 +2,8 @@ export const fundamentalsSection = {
   title: 'Fundamentals',
   routes: [
     { text: 'Dashboard', url: '/' },
-    // { text: 'ngrx basics', url: '/ngrx-basics' },
-    // { text: 'ngrx async flow', url: '/ngrx-async-flow' },
+    { text: 'ngrx basics', url: '/ngrx-basics' },
+    { text: 'ngrx async flow', url: '/ngrx-async-flow' },
   ],
   icon: {
     value: 'construction'
