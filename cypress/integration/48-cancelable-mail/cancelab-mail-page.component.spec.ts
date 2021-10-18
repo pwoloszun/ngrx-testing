a1d1e37b793d60643d1c036088107b25596f0f1a
pwoloszun <piotr.woloszun@gmail.com>
2021-10-18 15:29:33 +0200

    cancelable mail p1

--- cypress/integration/48-cancelable-mail/cancelab-mail-page.component.spec.ts
+++ cypress/integration/48-cancelable-mail/cancelab-mail-page.component.spec.ts
@@ -6,21 +6,29 @@ describe('cancelab-mail-page.component', () => {
 
   it.only('should successfully send cancelable and reversible mail', () => {
     // visit page
+    cy.visit('/cancelable-mail');
 
     // click Create Mail btn
+    cy.findByRole('button', { name: /Create Mail/i })
+      .click();
 
     // wihin dialog click Send btn
-
+    cy.findByRole('dialog')
+      .within(() => {
+        cy.findByRole('button', { name: /Send/i })
+          .click();
+      });
 
     //within snackbar text should contain /Sending/i
-    // cy.get('snack-bar-container')
-    //   .should('have.length', 1);
+    cy.get('snack-bar-container')
+      .should('have.length', 1)
+      .within(() => {
+        cy.findByText(/Sending/i);
+      });
 
     // ===
     //within snackbar text should contain /Mail has been sent/i
 
-
-    expect(true).to.deep.eq(false);
   });
 
   it.skip('should cancel sending mail', () => {
