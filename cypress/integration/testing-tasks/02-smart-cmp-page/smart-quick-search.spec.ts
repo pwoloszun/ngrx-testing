f5ac280c487fc8827f7473c37d0f92a547e62831
pwoloszun <piotr.woloszun@gmail.com>
2021-10-18 12:55:51 +0200

    smart quick search p2

--- cypress/integration/testing-tasks/02-smart-cmp-page/smart-quick-search.spec.ts
+++ cypress/integration/testing-tasks/02-smart-cmp-page/smart-quick-search.spec.ts
@@ -8,20 +8,45 @@ describe('smart-quick-search.component', () => {
 
   it.only('should perform quick search', () => {
     const pagePath = '/testing-tasks/smart-components';
+    const playersFixturePath = 'testing-tasks/02-smart-cmp-page/players.json';
+
+    cy.fixture(playersFixturePath)
+      .as('playersJson');
+
+    cy.intercept(
+      { method: 'GET', path: '/api/players?q=*' },
+      { fixture: playersFixturePath }
+    ).as('fetchPlayersRequest');
 
     // enter page
+    cy.visit('/testing-tasks/smart-components');
+
     // type search query into search field
+    cy.findByLabelText(/Search/i)
+      .type('some search txt');
+
     // check progressbar rendered
+    cy.findByRole('progressbar');
+
     // wait for request
+    cy.wait('@fetchPlayersRequest');
+
     // check progressbar not rendered
-    // check 'Search results' renderd
-
-    cy.findByText('costam')
+    cy.findByRole('progressbar')
       .should('not.exist');
-    // .type('asasa')
 
-    const playersFixturePath = 'testing-tasks/02-smart-cmp-page/players.json';
-    expect(true).to.deep.eq(false);
+    // check 'Search results' renderd
+    cy.findByText(/Search results/i)
+      .should('be.visible');
+
+    // list, listitem within
+
+    // assert listitems count === playersJson.length
+
+    // forEach: listitem.textContent "contains" playersJson[i].last_name
+    // forEach: listitem.textContent "contains" playersJson[i].first_name
+
+
   });
 
 });
