08cb91115a647e07bb7868e91fe284f4e5bda879
pwoloszun <piotr.woloszun@gmail.com>
2021-10-18 12:25:42 +0200

    quick search spec steps

--- cypress/integration/testing-tasks/02-smart-cmp-page/smart-quick-search.spec.ts
+++ cypress/integration/testing-tasks/02-smart-cmp-page/smart-quick-search.spec.ts
@@ -9,6 +9,17 @@ describe('smart-quick-search.component', () => {
   it.only('should perform quick search', () => {
     const pagePath = '/testing-tasks/smart-components';
 
+    // enter page
+    // type search query into search field
+    // check progressbar rendered
+    // wait for request
+    // check progressbar not rendered
+    // check 'Search results' renderd
+
+    cy.findByText('costam')
+      .should('not.exist');
+    // .type('asasa')
+
     const playersFixturePath = 'testing-tasks/02-smart-cmp-page/players.json';
     expect(true).to.deep.eq(false);
   });
