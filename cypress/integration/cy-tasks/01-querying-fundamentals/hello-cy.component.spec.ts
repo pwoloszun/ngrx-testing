9bdfaba9b5d7b141929ccf333e7444eaea2a993a
pwoloszun <piotr.woloszun@gmail.com>
2021-10-18 09:43:47 +0200

    hello spec p1

--- cypress/integration/cy-tasks/01-querying-fundamentals/hello-cy.component.spec.ts
+++ cypress/integration/cy-tasks/01-querying-fundamentals/hello-cy.component.spec.ts
@@ -1,4 +1,5 @@
 // import { visitQueryingPage } from './helpers';
+import { interval } from 'rxjs';
 
 describe('cy tasks: querying fundamentals > HelloCy component', () => {
 
@@ -6,11 +7,28 @@ describe('cy tasks: querying fundamentals > HelloCy component', () => {
     // TODO refactor
   });
 
-  it.skip('should render page heading', () => {
+  it('should render page heading', () => {
+    cy.visit('/cy-tasks/querying');
+
+    cy.findByText(/Hello Cy/i)
+      .should('exist')
+      .should('be.visible')
+      .then(($h3El) => {
+        console.log('then qq:', $h3El);
+        expect($h3El).to.have.length(1);
+        // return 123;
+      });
+
+    cy.wrap(true)
+      .should('deep.equal', true);
+
+  });
+
+  xit('should render page heading', () => {
     expect(true).to.deep.eq(false);
   });
 
-  it.skip('should mutate list item on btn click', () => {
+  xit('should mutate list item on btn click', () => {
     expect(true).to.deep.eq(false);
   });
 
