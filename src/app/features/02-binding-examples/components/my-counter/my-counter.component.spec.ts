f0daa2384fb346f142d96d2dd243c7fd255c0da3
pwoloszun <piotr.woloszun@gmail.com>
2021-11-02 14:16:28 +0100

    first test

--- src/app/features/02-binding-examples/components/my-counter/my-counter.component.spec.ts
+++ src/app/features/02-binding-examples/components/my-counter/my-counter.component.spec.ts
@@ -4,17 +4,31 @@ import { spy } from 'sinon';
 
 import { SharedModule } from '@shared/shared.module';
 import { MyCounterComponent } from './my-counter.component';
+import { merge } from 'lodash';
+
 
 describe('MyCounterComponent', () => {
 
   describe('@Inputs()', () => {
 
-    xit('should render label', () => {
-      expect(false).toEqual(true);
+    it('should render label', async () => {
+      const label = 'my label';
+      const value = 123;
+
+      const props = generateProps({ label, value });
+      await renderComponent(props);
+
+      const labelEl = await screen.findByText(/my label/i, {}, { timeout: 1000 });
+
+      await screen.findByText(/123/i);
+
+      // expect(labelEl).toBeInTheDocument();
     });
 
-    xit('should render value', () => {
-      expect(false).toEqual(true);
+    xit('should render value', async () => {
+      // await renderComponent();
+
+      // await screen.findByText(/20/i);
     });
 
     xit('should render default value if undefined', () => {
@@ -35,3 +49,17 @@ describe('MyCounterComponent', () => {
 
   });
 });
+
+async function renderComponent(props: any) {
+  return render(MyCounterComponent, {
+    componentProperties: props
+  });
+}
+
+function generateProps(props = {}) {
+  const defaultProps = {
+    label: 'default label',
+    value: 100,
+  };
+  return merge({}, defaultProps, props);
+}
