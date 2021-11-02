79423abcc96bd50df04de006116730c9774b3e90
pwoloszun <piotr.woloszun@gmail.com>
2021-11-02 01:59:59 +0100

    v0.2.5

--- src/app/features/15-custom-pipes/pipes/animate-count-to-number.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AnimateCountToNumberPipe } from './animate-count-to-number.pipe';
-
-describe('AnimateCountToNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AnimateCountToNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
