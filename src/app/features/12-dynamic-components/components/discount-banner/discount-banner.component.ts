780ce11424ca8bcc83085496a90ddb325451d7fe
pwoloszun <piotr.woloszun@gmail.com>
2021-11-03 13:21:39 +0100

    disc banner

--- src/app/features/12-dynamic-components/components/discount-banner/discount-banner.component.ts
+++ src/app/features/12-dynamic-components/components/discount-banner/discount-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, ComponentRef, Input, OnInit, ViewChild, ViewContainerRef, OnDestroy, Type } from '@angular/core';
+import { Component, ComponentFactoryResolver, ComponentRef, Input, OnInit, ViewChild, ViewContainerRef, OnDestroy, Type, ChangeDetectorRef } from '@angular/core';
 import { forEach } from 'lodash';
 import { Discount } from '../discount';
 
@@ -20,10 +20,15 @@ export class DiscountBannerComponent implements OnInit, OnDestroy {
   @Input() discounts!: DiscountItem[];
 
   // TODO
+  @ViewChild('discountsCont', { read: ViewContainerRef })
+  private discountsCont!: ViewContainerRef;
 
   private intervalId: any;
 
-  constructor() { }
+  constructor(
+    private cd: ChangeDetectorRef,
+    private resolver: ComponentFactoryResolver
+  ) { }
 
   ngOnInit() {
     this.loadDiscount();
@@ -36,8 +41,22 @@ export class DiscountBannerComponent implements OnInit, OnDestroy {
   loadDiscount() {
     let index = 0;
     this.intervalId = setInterval(() => {
-      // TODO
-      console.log('loadDiscounts TODO');
+      if (!this.discounts) {
+        return;
+      }
+      index = (index + 1) % this.discounts.length;
+      const { componentClass, inputValues } = this.discounts[index];
+
+      const comFactory = this.resolver.resolveComponentFactory(componentClass);
+
+      this.discountsCont.clear();
+      const comppRef = this.discountsCont.createComponent(comFactory);
+      const compInstance = comppRef.instance;
+
+      for (const key in inputValues) {
+        const value = inputValues[key];
+        (compInstance as any)[key] = value;
+      }
     }, 2000);
   }
 
