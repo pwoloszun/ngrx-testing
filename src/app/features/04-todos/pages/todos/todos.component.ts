780f9f500f599ddf27861d05733209954d457b7f
pwoloszun <piotr.woloszun@gmail.com>
2021-12-16 16:40:36 +0100

    todos p1

--- src/app/features/04-todos/pages/todos/todos.component.ts
+++ src/app/features/04-todos/pages/todos/todos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { TodoFormVm } from '../../components/todos-form/todos-form.component';
+import { TODOS_DATA } from '../../fake-data/todos-data';
 import { MyManageTodosService, Todo } from '../../services/my-manage-todos.service';
 
 @Component({
