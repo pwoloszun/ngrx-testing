925122a5ecb8fb580997d3dfb396e2deec966b4b
pwoloszun <piotr.woloszun@gmail.com>
2022-01-10 12:54:19 +0100

    car config fix

--- src/app/features/03-inputs-outputs-examples/components/car-configurator/car-configurator.component.ts
+++ src/app/features/03-inputs-outputs-examples/components/car-configurator/car-configurator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IOptionPickerOption } from '../option-picker/option-picker.component';
+
 @Component({
   selector: 'nts-car-configurator',
   templateUrl: './car-configurator.component.html',
@@ -8,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class CarConfiguratorComponent implements OnInit {
 
   // TODO
-  engines = ['Petrol', 'Diesel', 'Tesla'];
-  colors = ['Black', 'White', 'Red', 'Yellow'];
-  driveWheels = ['4X', 'Front-Wheel-Drive', 'Rear-Wheel-Drive'];
+  engines: IOptionPickerOption[] = [
+    { id: 100, value: 'Petrol' },
+    { id: 200, value: 'Diesel' },
+    { id: 300, value: 'Tesla' },
+  ];
+
+  colors: IOptionPickerOption[] = [
+    { id: 100, value: 'Black' },
+    { id: 200, value: 'White' },
+    { id: 300, value: 'Red' },
+    { id: 400, value: 'Yellow' },
+
+  ];
+
+  driveWheels: IOptionPickerOption[] = [
+    { id: 100, value: '4X' },
+    { id: 200, value: 'Front-Wheel-Drive' },
+    { id: 300, value: 'Rear-Wheel-Drive' },
+  ];
 
   ngOnInit() {
   }
