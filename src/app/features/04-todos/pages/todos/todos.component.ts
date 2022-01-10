8dd9bdd1fd306c02f48d51cbb88b557cd380f304
pwoloszun <piotr.woloszun@gmail.com>
2022-01-10 14:46:27 +0100

    todos task p1

--- src/app/features/04-todos/pages/todos/todos.component.ts
+++ src/app/features/04-todos/pages/todos/todos.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { TodoFormValues } from '../../components/todos-form/todos-form.component';
+import { TODOS_DATA } from '../../fake-data/todos-data';
 
 @Component({
   selector: 'nts-todos',
   templateUrl: './todos.component.html',
 })
 export class TodosComponent implements OnInit {
+
+  todos = TODOS_DATA;
 
   constructor() { }
 
