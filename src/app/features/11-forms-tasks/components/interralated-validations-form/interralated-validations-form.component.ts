93d4b6f8e30ad9f18f7b44be7ed6d19f75aefdd1
pwoloszun <piotr.woloszun@gmail.com>
2022-01-12 08:25:39 +0100

    interrelated form validations task

--- src/app/features/11-forms-tasks/components/interralated-validations-form/interralated-validations-form.component.ts
+++ src/app/features/11-forms-tasks/components/interralated-validations-form/interralated-validations-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormArray, FormBuilder, FormControl } from '@angular/forms';
 
-// import { startsWithCapitalLetterValidator } from './validators/starts-with-capital-letter.validator';
 import { defaultNameValidator } from '../../validators/default-name.validator';
 import { dobNationalIdValidator } from '../../validators/dob-national-id.validator';
 
@@ -13,10 +12,10 @@ import { dobNationalIdValidator } from '../../validators/dob-national-id.validat
 export class InterralatedValidationsFormComponent implements OnInit {
 
   myForm = this.fb.group({
-    fullName: ['', [defaultNameValidator]],
-    dateOfBirth: ['', Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)],
-    nationalId: ['', Validators.pattern(/^\d{11}$/)],
-  }, { validators: [dobNationalIdValidator] });
+    fullName: [''], // TODO validation: defaultname validator
+    dateOfBirth: [''], // TODO validation: yyyy-mm-dd pattern
+    nationalId: [''], // TODO validation: 11 digits
+  }); // TODO: form validation DoB to national id validation
 
   // helpers
   get fullNameCtrl(): FormControl {
@@ -31,11 +30,9 @@ export class InterralatedValidationsFormComponent implements OnInit {
     return this.myForm.get('nationalId') as FormControl;
   }
 
-  constructor(private fb: FormBuilder) {
-  }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   submitHandler() {
     console.log('form value', this.myForm.value);
