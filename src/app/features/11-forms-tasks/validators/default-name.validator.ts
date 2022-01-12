a840306330e461d448f197dd6e242862cc918bcf
pwoloszun <piotr.woloszun@gmail.com>
2022-01-12 15:35:09 +0100

    default name validator task p2

--- src/app/features/11-forms-tasks/validators/default-name.validator.ts
+++ src/app/features/11-forms-tasks/validators/default-name.validator.ts
@@ -1,17 +1,24 @@
-import { ValidatorFn, Validators } from '@angular/forms';
+import { ValidatorFn, Validators, AbstractControl } from '@angular/forms';
 
 import { startsWithCapitalLetterValidator } from './starts-with-capital-letter.validator';
 
-export const defaultNameValidator: ValidatorFn = (control) => {
+export const defaultNameValidator: ValidatorFn = (control: AbstractControl) => {
   const requiredValidationResult = Validators.required(control);
   // TODO: min length 3
   // TODO: starts with capital letter
+
+  // TODO
+  // '...man' - required man suffix
+
+  // 'batman'
+  // 'robman'
+  // 'robin' // invalid
 
   //  TODO return merged validation results
   // return null;
 
   const result = {
-    invalidDefaultName: true
+    invalidDefaultName: { message: `Does not match man suffix` }
   };
 
   return {
