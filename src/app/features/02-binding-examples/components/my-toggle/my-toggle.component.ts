009569684a879c68323aaa7020ee227b2b9a6b60
pwoloszun <piotr.woloszun@gmail.com>
2022-01-17 12:43:25 +0100

    minors

--- src/app/features/02-binding-examples/components/my-toggle/my-toggle.component.ts
+++ src/app/features/02-binding-examples/components/my-toggle/my-toggle.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+function selectBtnCssClasses(isVisible: boolean): any {
+  return isVisible ? { 'mat-primary': true } : { 'mat-warn': true };
+}
+
 @Component({
   selector: 'nts-my-toggle',
   templateUrl: './my-toggle.component.html',
@@ -12,7 +17,7 @@ export class MyToggleComponent implements OnInit {
 
   // state derived/computed
   get btnCssClasses(): any {
-    return this.isVisible ? { 'mat-primary': true } : { 'mat-warn': true };
+    return selectBtnCssClasses(this.isVisible);
   }
 
   get btnText(): string {
@@ -27,3 +32,30 @@ export class MyToggleComponent implements OnInit {
   }
 
 }
+
+
+
+
+
+class MyPerson {
+
+  private _name = 'bob';
+
+  get name() {
+    return this._name.toUpperCase();
+  }
+
+  set name(value: string) {
+
+  }
+
+}
+
+// client code
+const bob = new MyPerson();
+console.log('name:', bob.name);
+
+bob.name = 'ed';
+
+
+
