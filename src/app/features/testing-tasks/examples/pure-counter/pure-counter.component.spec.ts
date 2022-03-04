f273fc4a2fd4bdfab1b9dc1bbd7fec25063cfa19
pwoloszun <piotr.woloszun@gmail.com>
2022-03-04 10:26:21 +0100

    pure counter spec1

--- src/app/features/testing-tasks/examples/pure-counter/pure-counter.component.spec.ts
+++ src/app/features/testing-tasks/examples/pure-counter/pure-counter.component.spec.ts
@@ -6,10 +6,36 @@ import { PureCounterComponent } from './pure-counter.component';
 
 describe('PureCounterComponent', () => {
 
-  xit('should TODO', () => {
+  it('should render counter with received value', async () => {
+    const props = generateProps({ value: 123 });
+    await renderCompoonent(props);
+
+    const valueEl = await screen.findByText(/Value:/i);
+
+    expect(valueEl).toHaveTextContent(/Value: 123/i);
+    // expect(true).toEqual(false);
+  });
+
+  xit('should TODO1', () => {
     expect(true).toEqual(false);
   });
 
 });
 
 type Props = Partial<PureCounterComponent>;
+
+async function renderCompoonent(componentProperties: Props) {
+  await render(PureCounterComponent, {
+    componentProperties
+  });
+}
+
+function generateProps(props: Props = {}): Props {
+  const value = 200;
+  // const increment: any = () => { };
+  const defaultProps: Props = {
+    value,
+    // increment,
+  };
+  return merge({}, defaultProps, props);
+}
